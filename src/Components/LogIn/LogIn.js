36e31bf5ff27d538b810ee4d37c43a1be8e239f9
Ratna-Rani-Kundu <melaashopbd@gmail.com>
2021-10-19 11:51:03 +0600

    working on log in

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -2,12 +2,42 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from "@fortawesome/free-brands-svg-icons"
 import Button from '@restart/ui/esm/Button';
+import initializeAuthentication from '../../Firebase/firebase.init';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const icon=<FontAwesomeIcon icon={faGoogle} />
+initializeAuthentication()
+const googoleProvider=new GoogleAuthProvider();
+
 const LogIn = () => {
+    const auth = getAuth();
+  const  handleGoogleSignIn=()=>{
+    signInWithPopup(auth,googoleProvider)
+    .then(result=>{
+       // The signed-in user info.
+    const user = result.user;  
+    console.log(user)
+    })
+}
+const handleRegistration=(e)=>{
+console.log('registrations')
+e.preventDefault()
+}
     return (
-        <div>
-            <Button className='bg-danger'>{icon}  Google Log In</Button>
+        <div  className='container'>
+         
+                <form onSubmit={handleRegistration}>
+                    <h3>Please Register</h3>
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" name='email' />
+                    <br /><br /><br />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name='password' />
+                    <br /><br /><br />
+                   <input type="submit" value='Register'/>
+                </form>
+            <div>----------------------</div>
+           <br /><br /><br /> <Button onClick={handleGoogleSignIn} className='bg-primary'>{icon}  Google Sign In</Button>
         </div>
     );
 };
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+//web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCXS4hhruQNPreKgSdGzWhS0KTgHvQF0PM",
+    authDomain: "relive-ee8b8.firebaseapp.com",
+    projectId: "relive-ee8b8",
+    storageBucket: "relive-ee8b8.appspot.com",
+    messagingSenderId: "163471399952",
+    appId: "1:163471399952:web:8d3024771c71c1035f3e95"
+  };
+  export default firebaseConfig;
